44f8c40536f1339a967673012efd86c234a1cfb9
wassan128 <wassan.taiko.master@gmail.com>
2019-05-10 22:02:45 +0900

    [update] refactor

--- chirashi.go
+++ chirashi.go
@@ -2,42 +2,89 @@ package main
 
 import (
     "fmt"
+    "log"
     "io/ioutil"
-    "os"
+    "strconv"
     "strings"
 
     "github.com/PuerkitoBio/goquery"
 )
 
-func main() {
+type ShopInfo struct {
+    Id string
+    Name string
+    Chirashi *goquery.Document
+}
+type Item struct {
+    ImageUrl string
+    Name string
+    Price int
+}
+
+func openDoc() *goquery.Document {
     file, _ := ioutil.ReadFile("store2.html")
     reader := strings.NewReader(string(file))
 
     doc, err := goquery.NewDocumentFromReader(reader)
     if err != nil {
-        panic(err)
+        log.Fatal(err)
     }
 
-    items := doc.Find("a[id^=office_featured_product]")
-    if items.Size() <= 0 {
-        fmt.Printf("no chirashi information")
-        os.Exit(-1)
-    }
+    return doc
+}
 
+func GetShopInfo(id string) ShopInfo {
+    doc := openDoc()
+    name := doc.Find(".shop_name").Text()
+
+    var shop ShopInfo
+    shop.Id = id
+    shop.Name = name
+    shop.Chirashi = doc
+
+    return shop
+}
+
+func GetTokubaiInfo(shop ShopInfo) []Item {
+    items := shop.Chirashi.Find("a[id^=office_featured_product]")
+
+    var tokubaiItems []Item
     items.Each(func(_ int, item *goquery.Selection) {
+		var tokubaiItem Item
+
         // Thumb
         img := item.Find(".image")
         src, _ := img.Attr("data-src")
-        fmt.Println(src)
+        tokubaiItem.ImageUrl = src
 
         // Name
         name := strings.Trim(item.Find(".name").Text(), "\n")
-        fmt.Println(name)
+        tokubaiItem.Name = name
 
         // Price
         price := item.Find(".number").Text()
-        fmt.Println(price)
+        tokubaiItem.Price, _ = strconv.Atoi(strings.Replace(price, ",", "", -1))
 
-        fmt.Println(strings.Repeat("-", 20))
+        tokubaiItems = append(tokubaiItems, tokubaiItem)
     })
+
+    return tokubaiItems
 }
+
+func main() {
+    shop := GetShopInfo("")
+    items := GetTokubaiInfo(shop)
+    if len(items) == 0 {
+        fmt.Printf("%sの特売情報はありません", shop.Name)
+    }
+
+    fmt.Printf("%sの特売情報です\n---\n", shop.Name)
+    for _, item := range items {
+        fmt.Printf("「%s」が%d円で売られています。", item.Name, item.Price)
+        if len(item.ImageUrl) > 0 {
+            fmt.Printf("(%s)", item.ImageUrl)
+        }
+        fmt.Println("\n---")
+    }
+}
+
