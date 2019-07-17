a4e0fe2bb87bf966475f9e33bc3749e4401d8622
wassan128 <wassan.taiko.master@gmail.com>
2019-07-17 22:18:52 +0900

    Add comment for using api desc

--- shopinfo/code2address.go
+++ shopinfo/code2address.go
@@ -6,6 +6,7 @@ import (
     "github.com/PuerkitoBio/goquery"
 )
 
+/* 郵便番号検索API(https://api.nipponsoft.co.jp/zipcode/)を使用 */
 func Code2Address(zipCode string) string {
     doc, err := goquery.NewDocument("https://api.nipponsoft.co.jp/zipcode/?search=" + zipCode)
     if err != nil {
